481905976db58ec0cfd21d53dc88847b76c4f17d
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-01-18 18:02:06 +0500

    connected search with youtube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { Component } from "react";
 import SearchBar from "./components/searchbar";
-
+import YTSearch from "youtube-api-search";
+import VideoList from "./components/videolist";
 const API_KEY = "AIzaSyCY6Tz3rV2xq4NQgCoGDk3zM4iBtB5bjRQ";
 
-const App = function () {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={ videos : [] };
+    YTSearch({ key: API_KEY, term: "johny harris" },  (videos) => {
+      this.setState( {videos} );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
